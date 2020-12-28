5f12062b3c14ccb9736cf1227fe44299634ca5bb
cesaraugustopimenta <cesar.pimenta@alstratech.com>
2020-12-27 22:49:43 -0300

    'logica-ok'

--- dateframes.py
+++ dateframes.py
@@ -5,34 +5,15 @@ from utils import weekday_date
 inventory_avaibility = pd.read_csv('tvaberta_inventory_availability.csv', sep=';')
 program_audience = pd.read_csv('tvaberta_program_audience.csv')
 
-# print(inventory_avaibility['signal'])
-# print(inventory_avaibility['program_code'])
 
+df = pd.DataFrame({
+    'signal': pd.Series(inventory_avaibility['signal']),
+    'program_code': pd.Series(inventory_avaibility['program_code']),
+    'weekday': pd.Series(inventory_avaibility['date']),
+    'available_time': pd.Series(inventory_avaibility['available_time']),
+    'predicted_audience': '',
+})
 
-# for x in program_audience:
-#     print(x)
-#     # if x['program_code'] == 'SHOV':
-#     #     print(x['program_code'])
-#
-
-# print(inventory_avaibility.head(5))
-
-# program_code = str(input('> programa code : '))
-# data_program = str(input('> data do programa : '))
-
-# program_code = program_audience[program_audience.program_code == 'VALE']
-# program_signal = program_code[program_code.signal == 'SP1']
-# order_date = program_signal.sort_values('exhibition_date')
-# last_programs = order_date[-4::]
-# clear_data = last_programs['exhibition_date'].values[0].replace('-', '')
-# # print(datetime.datetime(last_programs['exhibition_date'].values[0]))
-# print(weekday_date(clear_data))
-
-# print((last_programs['average_audience'].sum() / 4))
-
-# x = inventory_avaibility[inventory_avaibility.program_code == 'VALE']
-# print(x)
-new_df = pd.DataFrame({'signal': [inventory_avaibility['signal']], 'media': [], 'over': []}, ignore_index=True)
 for index, row in inventory_avaibility.iterrows():
     program_code = program_audience[program_audience.program_code == row['program_code']]
     program_signal = program_code[program_code.signal == row['signal']]
@@ -41,19 +22,10 @@ for index, row in inventory_avaibility.iterrows():
     for index_x, row_x in order_date.iterrows():
         if weekday_date(False, row['date'].replace('/', '')) == weekday_date(True, row_x['exhibition_date'].replace('-','')):
             media.append(row_x['average_audience'])
-    new_df.append({'media': sum(media[-4:])/4}, ignore_index=True)
-print(new_df)
+    df.loc[index, 'predicted_audience'] = sum(media[-4:])/4
 
 
-# print(program_audience[::1000])
-#
-# for x in program_audience.iterrows():
-#     print(program_audience[(1+x[0])])
+# def exact_program()
 
-# print(y[y.exhibition_date == data_programa])
-# # print(y[(y.signal == 'SP1') & (y.exhibition_date == '2020-06-06')])
-
-# print(program_audience.head(5))
-
-# Função 'loc' do pandas, localização - [(periodo que nem slice), [(colunas de escolha do data frame)]]
-# print(inventory_avaibility.loc[:, ['signal', 'program_code']])
+# mask = (df['weekday'] >= '25/08/2020') & (df['weekday'] <= '30/08/2020')
+print(df[(df['weekday'] >= '25/08/2020') & (df['weekday'] <= '30/08/2020')])
